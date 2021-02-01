06580daddd34210ba963f0a602b61bce7001bcc2
Tim Miller <tim.miller@risk.lexisnexis.com>
2021-02-01 13:23:03 -0500

    add example

---
+++ example/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=2.32.0"
+    }
+  }
+  required_version = ">=0.13.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_subscription" "current" {
+}
+
+module "subscription" {
+  source = "github.com/Azure-Terraform/terraform-azurerm-subscription-data.git?ref=v1.0.0"
+  subscription_id = data.azurerm_subscription.current.subscription_id
+}
+
+module "naming" {
+  source = "github.com/Azure-Terraform/example-naming-template.git?ref=v1.0.0"
+}
+
+module "metadata" {
+  source = "github.com/Azure-Terraform/terraform-azurerm-metadata.git?ref=v1.1.0"
+
+  naming_rules = module.naming.yaml
+
+  market              = "us"
+  project             = "https://github.com/Azure-Terraform/terraform-azurerm-kubernetes/tree/master/example/mixed-arch"
+  location            = "eastus2"
+  environment         = "sandbox"
+  product_name        = "contosoweb"
+  business_unit       = "infra"
+  product_group       = "contoso"
+  subscription_id     = module.subscription.output.subscription_id
+  subscription_type   = "dev"
+  resource_group_type = "app"
+
+  additional_tags = {
+    "support_email" = "support@contoso.com"
+    "owner"         = "Jon Doe"
+  }
+}
+
+output "names" {
+  value = module.metadata.names
+}
+
+output "tags" {
+  value = module.metadata.tags
+}
